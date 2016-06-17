a7abbe1de81c5ccf79daf2ad02c1a16f2dbe54e1
xiaoming <henyan94@gmail.com>
2016-06-17 11:21:45 +0800

    Added some routing stuff with comments and articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   resource :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
