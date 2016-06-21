5f11de952a00d44c7355f63a895b869533b43e63
xiaoming <henyan94@gmail.com>
2016-06-21 17:25:40 +0800

    Added a create user action in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You are now registered"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Invalid input"
+      flash[:color] = "invalid"
+    end
+    render "new"
   end
 end
