b452a8a9c0879d961525bf1f7d7e9affb6166bc3
xiaoming <henyan94@gmail.com>
2016-06-21 17:02:34 +0800

    Started doing some user auth stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617080227) do
+ActiveRecord::Schema.define(version: 20160621090123) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20160617080227) do
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.string   "email",      limit: 255
+    t.string   "salt",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_foreign_key "comments", "articles"
