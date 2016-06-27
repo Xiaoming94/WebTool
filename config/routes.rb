cb4fa57e0f79d6ad4b04c838a6aea22080e50d17
xiaoming <henyan94@gmail.com>
2016-06-27 15:21:09 +0800

    Now pages are requested by title

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   match 'adminpanel/:action' => 'adminpanel#:action', via: [:get]
 
-  resources :pages
   get 'pages/:title', to: 'pages#show'
+  resources :pages
   resources :articles do 
     resources :comments
   end
