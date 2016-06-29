c13085cd5d492e67b5a6a39d23e05b97f4e37d03
xiaoming <henyan94@gmail.com>
2016-06-29 16:49:50 +0800

    Changed some attributes to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160629080552) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
+    t.string   "username",   limit: 255
     t.string   "email",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
