b553ff1f2e6c32a86607676145ed472670febafd
xiaoming <henyan94@gmail.com>
2016-06-29 17:26:53 +0800

    Added a line for has_secure_password in users model, started working on passwords of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ActiveRecord::Base
     length: {maximum: 255},
     uniqueness: true,
     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629091448) do
+ActiveRecord::Schema.define(version: 20160629092547) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -40,11 +40,12 @@ ActiveRecord::Schema.define(version: 20160629091448) do
   add_index "pages", ["title"], name: "index_pages_on_title", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "username",   limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",            limit: 255
+    t.string   "username",        limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
