261ed0571eb7635ec5a93ec39fdc47851de8e786
xiaoming <henyan94@gmail.com>
2016-06-29 17:13:28 +0800

    Added unique indexes to pages title and users email and username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629080552) do
+ActiveRecord::Schema.define(version: 20160629091448) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160629080552) do
     t.datetime "updated_at",               null: false
   end
 
+  add_index "pages", ["title"], name: "index_pages_on_title", unique: true, using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "username",   limit: 255
@@ -44,6 +46,9 @@ ActiveRecord::Schema.define(version: 20160629080552) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "comments", "articles"
 end
