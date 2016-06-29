add3925264772e4dbcc9276b0709eed904df2b26
xiaoming <henyan94@gmail.com>
2016-06-29 17:39:44 +0800

    Added password validations to user on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,5 @@ class User < ActiveRecord::Base
     uniqueness: true,
     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
