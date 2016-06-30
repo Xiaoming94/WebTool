7a5a110c0d5766315f6216f22da76ad4a548fb93
xiaoming <henyan94@gmail.com>
2016-06-30 15:45:27 +0800

    Now one can authenticate and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(:username => params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to url_for(:controller => :webpage, :action => :index)
+    else
+      flash[:danger] = 'Invalid User/Password combination'
+      render 'new'
+    end
   end
 
   def destroy
