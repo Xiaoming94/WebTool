b94017b79674ad6761707c4c6a53450d8fdff322
xiaoming <henyan94@gmail.com>
2016-06-30 14:27:47 +0800

    Creating a user works now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to url_for(controller: webpage, action: index)
+      redirect_to url_for(:controller => :webpage, :action => :index)
     else
       render 'new'
     end
