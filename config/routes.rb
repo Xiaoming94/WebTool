16223c2014163814fa1aec02d03c89705f839668
xiaoming <henyan94@gmail.com>
2016-06-30 14:39:39 +0800

    Added routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   get 'adminpanel', to: 'adminpanel#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'pages/:title', to: 'pages#showbytitle'
   namespace :admin do
