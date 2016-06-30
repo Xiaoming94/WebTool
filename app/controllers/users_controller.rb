807267ea91e1ad18e96726a456ea77c1ffe5f61c
xiaoming <henyan94@gmail.com>
2016-06-30 11:51:59 +0800

    created a working create method for users in users_controller class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle success later
+    else
+      render 'new'
+    end
+  end
 end
