5245e3bb6cc5261d50b1ce138d9c37b1134dccc2
xiaoming <henyan94@gmail.com>
2016-06-30 11:01:19 +0800

    Added routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'webpage#index'
 
-  match 'users/:action' => 'users#:action', via: [:get,:post]
+  get 'signup', to: 'users#new'
   get 'adminpanel', to: 'adminpanel#index'
 
   get 'pages/:title', to: 'pages#showbytitle'
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
