c7c146bacc4bbf37c42291d806815d78a288b734
xiaoming <henyan94@gmail.com>
2016-06-30 11:53:37 +0800

    Added format validations for usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/i
   validates :name,
     presence: true,
     length: {maximum: 50}
   validates :username,
     presence: true,
     length: {maximum: 20},
-    uniqueness: true
+    uniqueness: true,
+    format: { with: VALID_USERNAME_REGEX }
   validates :email,
     presence: true,
     length: {maximum: 255},
