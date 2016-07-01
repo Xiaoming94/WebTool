ce21f480af6495157e978ed44ad95eec23fc9edb
xiaoming <henyan94@gmail.com>
2016-07-01 15:24:48 +0800

    Added a is logged_in method to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,9 @@ module SessionsHelper
     @current_user ||= User.find(session[:user_id])
   end
 
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   def logged_in?
     !current_user.nil?
   end
