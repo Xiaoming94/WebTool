0fd2c352a8bca692a832584b37b74bcef4450e7b
xiaoming <henyan94@gmail.com>
2016-07-01 16:35:05 +0800

    Added a method for logging out a user from current session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,9 @@ module SessionsHelper
     @current_user ||= User.find(session[:user_id])
   end
 
+  def log_out
+    session.delete[:user_id]
+  end
   def is_logged_in?
     !session[:user_id].nil?
   end
