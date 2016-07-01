e0127b1db253e9b06e2dc3414c0588405beee028
xiaoming <henyan94@gmail.com>
2016-07-01 16:06:49 +0800

    Corrected login redirect in base login

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BaseController < ApplicationController
   def index
     if !is_logged_in?
-      redirect_to url_for(:action => :login)
+      redirect_to url_for(admin_login_path)
     end
   end
   def login
