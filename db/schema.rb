1f301373adae22fe17cb10db139ed71b3ea744ac
xiaoming <henyan94@gmail.com>
2016-07-14 14:58:04 +0800

    Added a migration to add type column to files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711033346) do
+ActiveRecord::Schema.define(version: 20160714065700) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160711033346) do
     t.string   "attachment", limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "type",       limit: 255
   end
 
   create_table "users", force: :cascade do |t|
