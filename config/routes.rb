cf861582ae3b24b92e698342e0569d595419887e
xiaoming <henyan94@gmail.com>
2016-07-15 14:42:12 +0800

    Added images as resources in the routing table

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,12 @@ Rails.application.routes.draw do
     resources :pages
     resources :articles
     resources :resumes, :only => [:index, :new, :create, :destroy]
+    resources :images, :only => [:index, :new, :create, :destroy]
   end
   resources :articles , :only => [:index ,:show] do 
     resources :comments
   end
-  
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
