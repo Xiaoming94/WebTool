c78ef3fe500e6d54f742d0f6f94dcb0894be050a
xiaoming <henyan94@gmail.com>
2016-07-15 16:13:40 +0800

    Removed resumes controller

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,2 +1,29 @@
-class Admin::ImagesController < ApplicationController
+class Admin::ResumesController < Admin::BaseController
+  def index
+    @images = Resume.all
+  end
+
+  def new
+    @image = Resume.new
+  end
+
+  def create
+    @image = Resume.new(resume_params)
+    if @image.save
+      redirect_to admin_resumes_path, notice: "The resume #{@image.name} has been uploaded"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @image = Resume.find(params[:id])
+    @image.destroy
+    redirect_to admin_resumes_path, notice: "The resume #{@image.name} has been deleted"
+  end
+
+  private
+    def resume_params
+      params.require(:resume).permit(:name, :attachment)
+    end
 end
