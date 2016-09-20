05dd1fbcfc6cee44d0e91659b925c84a120d17be
xiaoming <henyan94@gmail.com>
2016-09-20 17:48:54 +0200

    Added redirects for admin view when no Admin User is present

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,9 +6,11 @@ class Admin::BaseController < ApplicationController
 
   def login
     if !admin_user_exists?
-      redirect_to
-    end
+      redirect_to new_admin_user_path
+    else
       render layout: "login"
+    end
+
   end
 
   def create
@@ -31,6 +33,11 @@ class Admin::BaseController < ApplicationController
     render :stop
   end
   private
+
+  def admin_user_exists?
+    return !User.find_by(is_admin: true).nil?
+  end
+
   def test_is_admin
     if !is_logged_in?
       redirect_to admin_login_path
